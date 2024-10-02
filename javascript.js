20785aa853695a0afa008877089af6d62c109a8f
Jared <jaredmgray@outlook.com>
2024-10-01 19:11:40 -0500

    got function of calc to work, need to get to display

---
+++ javascript.js
@@ -0,0 +1,106 @@
+const buttons = document.querySelectorAll('#input button')
+const display = document.querySelector(".display")
+
+let firstNumber = ""
+let secondNumber = ""
+let firstOperator = ""
+let result = ""
+
+function operate (){
+    if(firstNumber !== "" && firstOperator !== "" && secondNumber !== ""){
+        const num1 = parseFloat(firstNumber)
+        const num2 = parseFloat(secondNumber)
+
+        switch (firstOperator){
+            case "+":
+                result = num1 + num2 
+                firstNumber = result
+                secondNumber = ""
+                firstOperator = ""
+                break
+
+            case "-":
+                result = num1 - num2 
+                firstNumber = result
+                secondNumber = ""
+                firstOperator = ""
+
+                break
+
+            case "*":
+                result = num1 * num2 
+                firstNumber = result
+                secondNumber = ""
+                firstOperator = ""
+
+                break
+
+            case "/":
+                result = num1 / num2 
+                firstNumber = result
+                secondNumber = ""
+                firstOperator = ""
+
+                break
+        }
+    }
+
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let value = button.value
+
+        if(!isNaN(value)){
+            value = button.value
+
+            if (firstNumber === "" || firstOperator === ""){
+                firstNumber += value
+
+                const firstValue = document.createAttribute("div")
+                firstValue.textContent = firstNumber
+
+                console.log('first number', firstNumber)
+
+
+            } else if (firstNumber !== ""){
+                secondNumber += value
+                console.log('second number', secondNumber)
+
+            } else if (result !== ""){
+                secondNumber += value
+            }
+        } else if (
+
+            button.value === '+' || 
+            button.value === '-' || 
+            button.value === '*' || 
+            button.value === '/'
+        ) { 
+
+            let valueSign = button.value
+
+            if (firstOperator === ""){
+                firstOperator += valueSign
+                console.log(firstOperator)
+
+            } else if (firstNumber !== "" && firstOperator !== ""){
+
+                operate()
+                firstOperator += valueSign
+                
+                console.log(result)
+            }
+        } else if (button.value === '='){
+            operate()
+            console.log(result)
+
+        } else if (button.value === 'clear'){
+            firstNumber = ""
+            secondNumber = ""
+            firstOperator = ""
+            result = ""
+        }
+       
+    })
+})
