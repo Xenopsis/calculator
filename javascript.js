9aadbfcbe31891912db3f0091795396dbc21f190
Jared <jaredmgray@outlook.com>
2024-10-02 18:10:49 -0500

    updated / fixed bugs with multiple operators

--- javascript.js
+++ javascript.js
@@ -5,14 +5,19 @@ let firstNumber = ""
 let secondNumber = ""
 let firstOperator = ""
 let result = ""
+display.innerHTML = '0'
 
 function operate (){
+
     if(firstNumber !== "" && firstOperator !== "" && secondNumber !== ""){
+
         const num1 = parseFloat(firstNumber)
         const num2 = parseFloat(secondNumber)
 
         switch (firstOperator){
+
             case "+":
+
                 result = num1 + num2 
                 firstNumber = result
                 secondNumber = ""
@@ -20,6 +25,7 @@ function operate (){
                 break
 
             case "-":
+
                 result = num1 - num2 
                 firstNumber = result
                 secondNumber = ""
@@ -28,6 +34,7 @@ function operate (){
                 break
 
             case "*":
+
                 result = num1 * num2 
                 firstNumber = result
                 secondNumber = ""
@@ -36,6 +43,7 @@ function operate (){
                 break
 
             case "/":
+                
                 result = num1 / num2 
                 firstNumber = result
                 secondNumber = ""
@@ -57,19 +65,25 @@ buttons.forEach((button) => {
             if (firstNumber === "" || firstOperator === ""){
                 firstNumber += value
 
-                const firstValue = document.createAttribute("div")
+                display.innerHTML = ""
+                const firstValue = document.createElement("div")
+                firstValue.classList.add("number")
                 firstValue.textContent = firstNumber
-
-                console.log('first number', firstNumber)
-
+                display.appendChild(firstValue)
 
             } else if (firstNumber !== ""){
                 secondNumber += value
-                console.log('second number', secondNumber)
+
+                display.innerHTML = ""
+                const secondValue = document.createElement("div")
+                secondValue.classList.add("number")
+                secondValue.textContent = secondNumber
+                display.appendChild(secondValue)
 
             } else if (result !== ""){
                 secondNumber += value
             }
+
         } else if (
 
             button.value === '+' || 
@@ -80,34 +94,94 @@ buttons.forEach((button) => {
 
             let valueSign = button.value
 
-            if (firstOperator === ""){
-                firstOperator += valueSign
-                console.log(firstOperator)
+            if (firstOperator !== "" && secondNumber === ""){
+                
+                display.innerHTML = ""
+                const operatorValue = document.createElement("div")
+                operatorValue.classList.add("number")
+                operatorValue.textContent = button.textContent
+                display.appendChild(operatorValue)
+                firstOperator = valueSign
+            }
+            else if (firstOperator === ""){
+                firstOperator = valueSign
+
+                display.innerHTML = ""
+                const operatorValue = document.createElement("div")
+                operatorValue.classList.add("number")
+                operatorValue.textContent = button.textContent
+                display.appendChild(operatorValue)
 
             } else if (firstNumber !== "" && firstOperator !== ""){
 
                 operate()
-                firstOperator += valueSign
 
-                console.log(result)
+                display.innerHTML = ""
+                const operatorValue = document.createElement("div")
+                operatorValue.classList.add("number")
+                operatorValue.textContent = button.textContent
+                display.appendChild(operatorValue)
+                firstOperator = valueSign
+
+            } else if (firstNumber !== "" && secondNumber === ""){
+                display.innerHTML = ""
+                const operatorValue = document.createElement("div")
+                operatorValue.classList.add("number")
+                operatorValue.textContent = button.textContent
+                display.appendChild(operatorValue)
+                firstOperator = valueSign
             }
-        } else if (button.value === '='){
-            operate()
-            console.log(result)
 
+        } else if (button.value === '='){
+            if (firstOperator === "/" && secondNumber === "0"){
+                display.innerHTML = "Don't do that"
+
+            } else {
+                operate()
+                display.innerHTML = Math.round(result * 1000) / 1000
+            }
+            
         } else if (button.value === 'clear'){
+
+            display.innerHTML = "0"
             firstNumber = ""
             secondNumber = ""
             firstOperator = ""
             result = ""
-        } else if (button.value === '.'){
-            if (firstOperator === ""){
-                firstNumber += button.value
 
-            } else if (firstOperator !== ""){
-                secondNumber += button.value
+        } else if (button.value === 'delete') {
+
+            display.innerHTML = "0"
+
+            if (firstNumber !== '' && firstOperator === ''){
+                firstNumber = ""
+
+            } else if (firstOperator !== '' && secondNumber === ''){
+                firstOperator = ""
+                display.innerHTML = firstNumber
+
+            } else if (secondNumber !== '') {
+                secondNumber = ""
+                display.innerHTML = firstOperator
+            }
+
+        } else if (button.value === '.'){
+
+            if (firstOperator === ""){
+
+                if (!firstNumber.includes('.')){
+
+                    firstNumber += button.value
+                    display.innerHTML += '.'
+                }
+            } else {
+
+                if (!secondNumber.includes('.')){
+
+                    secondNumber += button.value
+                    display.innerHTML += '.'
+                }
             }
         }
-       
     })
 })
